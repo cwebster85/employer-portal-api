5915aff37988ca09df607468d74acaccba7ca3e2
cwebster85 <chriswebster1985@googlemail.com>
2025-05-10 10:38:17 +0100

    enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
